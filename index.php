618fc03eea21b19de3afbb0aa3c02b44ebcaddd8
Gustav Forselius <gf222cy@student.lnu.se>
2019-09-24 16:39:05 +0200

    adding date string and corresponding methods

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 //INCLUDE THE FILES NEEDED...
 require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -2,11 +2,42 @@
 
 class DateTimeView {
 
+	private $dateObj = "getDateObj();";
+
+	public function getWeekDay() {
+		return date("l");
+	}
+
+	public function getDateDay() {
+		return date("d");
+	}
+
+	public function getMonth() {
+		return date("F");
+	}
+
+	public function getYear() {
+		return date("Y");
+	}
+
+	public function getHour() {
+		return date("G");
+	}
+
+	public function getMinute() {
+		return date("i");
+	}
+
+	public function getSecond() {
+		return date("s");
+	}
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$dateTimeObj = getdate();
 
-		return '<p>' . $timeString . '</p>';
+		$timeString = '';
+
+		return '<p>' . $timeString . $this->getWeekDay() . ', the ' . $this->getDateDay() . 'th of ' . $this->getMonth() . ' ' . $this->getYear() . ', The time is ' . $this->getHour() . ':' . $this->getMinute() . ':' . $this->getSecond() . '</p>';
 	}
 }
